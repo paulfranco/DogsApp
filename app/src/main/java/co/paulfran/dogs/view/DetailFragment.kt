2bee601a523fd4300abc6adc78ae07508a44b4c6
Paul Franco <paulfranco@me.com>
2019-08-13 12:31:18 -0700

    screen layout and navigation functionality

---
+++ app/src/main/java/co/paulfran/dogs/view/DetailFragment.kt
@@ -0,0 +1,33 @@
+package co.paulfran.dogs.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import co.paulfran.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonList.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/co/paulfran/dogs/view/ListFragment.kt
@@ -0,0 +1,36 @@
+package co.paulfran.dogs.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+
+import co.paulfran.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetail.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+
+        }
+
+    }
+
+
+}
