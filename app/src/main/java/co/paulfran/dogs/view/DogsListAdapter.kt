178c0c26d92e9ebd97d415503a119cbb1d72db93
Paul Franco <paulfranco@me.com>
2019-08-16 18:29:07 -0700

    building the list adapter

---
+++ app/src/main/java/co/paulfran/dogs/view/DogsListAdapter.kt
@@ -0,0 +1,36 @@
+package co.paulfran.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.paulfran.dogs.R
+import co.paulfran.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>) : RecyclerView.Adapter<DogsListAdapter.DogViewHolder>(){
+
+    fun updateDogList(newDogsList: List<DogBreed>) {
+        dogsList.clear()
+        dogsList.addAll(newDogsList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+
+    }
+
+    override fun getItemCount() = dogsList.size
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        holder.view.name.text = dogsList[position].dogBreed
+        holder.view.lifespan.text = dogsList[position].lifeSpan
+
+    }
+
+    class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+}
