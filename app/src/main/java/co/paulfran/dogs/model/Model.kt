b3635940ac3556ce401532eba6731cfb74a82ede
Paul Franco <paulfranco@me.com>
2019-08-18 14:11:20 -0700

    set up Retrofit

--- app/src/main/java/co/paulfran/dogs/model/Model.kt
+++ app/src/main/java/co/paulfran/dogs/model/Model.kt
@@ -1,11 +1,20 @@
 package co.paulfran.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
+        @SerializedName("id")
         val breedId: String?,
+        @SerializedName("name")
         val dogBreed: String?,
+        @SerializedName("lifespan")
         val lifeSpan: String?,
+        @SerializedName("breed_group")
         val breedGroup: String?,
+        @SerializedName("bred_for")
         val bredFor: String?,
+        @SerializedName("temperament")
         val temperament: String?,
+        @SerializedName("url")
         val imageUrl: String?
 )
