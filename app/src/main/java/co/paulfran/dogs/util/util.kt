9097cd733839a3107f0f447c77254439b830148e
Paul Franco <paulfranco@me.com>
2019-09-27 12:54:27 -0700

    bind image

--- app/src/main/java/co/paulfran/dogs/util/util.kt
+++ app/src/main/java/co/paulfran/dogs/util/util.kt
@@ -3,6 +3,7 @@ package co.paulfran.dogs.util
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import co.paulfran.dogs.R
 import com.bumptech.glide.Glide
@@ -24,4 +25,9 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
             .setDefaultRequestOptions(options)
             .load(uri)
             .into(this)
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
 }
